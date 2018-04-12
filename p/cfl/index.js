d5ee38a484e7da9f608ea45e598cfedaff61c5a4
Jannis R <mail@jannisr.de>
2018-04-12 21:25:40 +0200

    CFL profile :sparkles:

---
+++ p/cfl/index.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const products = require('./products')
+
+const transformReqBody = (body) => {
+	body.client = {
+		type: 'IPH',
+		id: 'HAFAS',
+		v: '4000000',
+		name: 'cflPROD-STORE',
+		os: 'iPhone OS 9.3.5'
+	}
+	body.ver = '1.16'
+	body.auth = {aid: 'ALT2vl7LAFDFu2dz'}
+	body.lang = 'de'
+
+	return body
+}
+
+const insaProfile = {
+	locale: 'de-DE',
+	timezone: 'Europe/Berlin',
+	// todo: use HTTPS, ignore invalid cert
+	endpoint: 'http://horaires.cfl.lu/bin/mgate.exe',
+	transformReqBody,
+
+	products: products,
+
+	journeyLeg: true,
+	radar: true
+}
+
+module.exports = insaProfile;
---
+++ p/cfl/products.js
@@ -0,0 +1,37 @@
+'use strict'
+
+module.exports = [
+	// todo: other bits
+	{
+		id: 'express-train',
+		mode: 'train',
+		bitmasks: [1, 2],
+		name: 'TGV, ICE, EuroCity',
+		short: 'TGV/ICE/EC',
+		default: true
+	},
+	{
+		id: 'local-train',
+		mode: 'train',
+		bitmasks: [8, 16],
+		name: 'local trains',
+		short: 'local',
+		default: true
+	},
+	{
+		id: 'tram',
+		mode: 'train',
+		bitmasks: [256],
+		name: 'Tram',
+		short: 'Tram',
+		default: true
+	},
+	{
+		id: 'gondola',
+		mode: 'gondola',
+		bitmasks: [512],
+		name: 'Fun', // taken from the horaires.cfl.lu website
+		short: 'Fun',
+		default: true
+	}
+]
